b8095a57f97c3300b09565ee91747b91ffe0c2c8
konstalbristers <kcparis33@gmail.com>
2025-07-28 16:24:40 -0700

    added view file with form and foal status output

---
+++ views/goal_view.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head><title>Goal Tracker</title></head>
+<body>
+    <h1>Personal Goal Tracker</h1>
+    <p><?= $goal->getStatus(); ?></p>
+
+    <form method="POST">
+        <label>Goal Name: 
+            <input type="text" name="name" value="<?php htmlspecialchars($goal->name); ?>">
+        </label><br>
+
+        <label>Target: 
+            <input type="number" name="target" value="<?php htmlspecialchars($goal->target); ?>">
+        </label><br>
+
+        <label>Add Progress: 
+            <input type="number" name="progress">
+        </label><br>
+
+        <button type="submit">Update</button>
+    </form>
+</body>
+</html>
