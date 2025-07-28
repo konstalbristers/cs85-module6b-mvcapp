1aaa2332dfc5bdecb034a5ef3f0af357b52ed338
konstalbristers <kcparis33@gmail.com>
2025-07-28 16:21:12 -0700

    added foal model with properties and business logic

---
+++ src/Models/Goal.php
@@ -0,0 +1,26 @@
+<?php
+namespace App\Models;
+
+class Goal {
+    public $name;
+    public $target;
+    public $progress = 0;
+
+    public function __construct($name, $target) {
+        $this->name = $name;
+        $this->target = $target;
+    }
+
+    public function addProgress($amount) {
+        $this->progress += $amount;
+        if ($this->progress > $this->target) {
+            $this->progress = $this->target;
+        }
+    }
+
+    public function getStatus() {
+        $percentage = ($this->progress / $this->target) * 100;
+        return "{$this->name}: {$this->progress}/{$this->target} (" . round($percentage) . "% complete)";
+    }
+}
+?>
